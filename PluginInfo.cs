d2cb67710d1a92be9cd03340511f4f7a2c1353b8
Silent <idk7778001@gmail.com>
2025-03-11 00:41:24 +0000

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,7 +4,7 @@ namespace iiMenu
     {
         public const string GUID = "org.iidk.gorillatag.iimenu";
         public const string Name = "ii's Stupid Menu";
-        public const string Description = "Created by @goldentrophy with love <3";
-        public const string Version = "5.6.1";
+        public const string Description = "Created by @goldentrophy with love <3 Fixed by silent till iidk updates";
+        public const string Version = "5.6.2";
     }
 }
